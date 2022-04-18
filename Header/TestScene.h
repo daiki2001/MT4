02cd4540a49adda51549bd420de0b370b72e18ec
daiki2001 <k020g1234@g.neec.ac.jp>
2022-04-18 18:50:48 +0900

    自由落下

--- Header/TestScene.h
+++ Header/TestScene.h
@@ -1,29 +1,20 @@
 #pragma once
 #include "./Header/BaseScene.h"
 #include "./Math/EngineMath.h"
-#include "./Math/Collision/Collision.h"
-#include "Player.h"
 
 class TestScene : public BaseScene
 {
 public: // エイリアス
 	using XMFLOAT4 = DirectX::XMFLOAT4;
-	using XMMATRIX = DirectX::XMMATRIX;
+	using Matrix4 = EngineMath::Matrix4;
 	using Vector3 = EngineMath::Vector3;
 
 public: // 定数
 	static const float sphereRadius; //球体オブジェクトの半径
-	static const float planeRadius;  //平面オブジェクトの大きさ
-	static const Vector3 tPos1;      //三角形オブジェクトの頂点1
-	static const Vector3 tPos2;      //三角形オブジェクトの頂点2
-	static const Vector3 tPos3;      //三角形オブジェクトの頂点3
 
-	static const XMFLOAT4 color[3];
+	static const XMFLOAT4 color; //オブジェクトの色
 
-	static const float rayLength; //レイの長さ
-
-	static const float triangleAngle;
-	static const float triangleScale;
+	static const float gravity; //重力
 
 public: // メンバ関数
 	TestScene(SceneChenger* sceneChenger);
@@ -35,39 +26,14 @@ public: // メンバ関数
 private: // メンバ変数
 	int background; //背景画像
 
-	// オブジェクトの生成
-	int sphere;   //球体オブジェクト
-	int plane;    //平面オブジェクト
-	int triangle; //三角形オブジェクト
-	int ray;      //レイ描画用オブジェクト
+	int playerObject;    //プレイヤーの描画オブジェクト
 
-	Player* player;
-	Vector3 playerPos;
+	Vector3 playerPos;      //プレイヤーの座標
+	Matrix4 playerRota;     //プレイヤーの回転行列
+	Vector3 playerVel;      //プレイヤーの速度
+	Vector3 playerAccel;    //プレイヤーの加速度
+	Vector3 playerAddForce; //プレイヤーに加わる力
 
-	Vector3 rayPos;
-	Vector3 rayDir;
-	Vector3 rayAngle;
-	XMMATRIX rayRot;
-	float rayDistance;
-
-	Vector3 spherePos;
-
-	Vector3 planePos;
-	XMMATRIX planeRot;
-	float planeScale;
-	Vector3 planeToRayHitPos;
-
-	Vector3 trianglePos;
-	XMMATRIX triangleRot;
-	XMMATRIX triangleInvRot;
-	XMMATRIX triangleMatrix;
-
-	Sphere playerCollision;
-	Sphere sphereCollision;
-	Plane planeCollision;
-	Triangle triangleCollision;
-	Ray rayCollision;
-
-	bool isCameraMove;
-	bool isRay;
+	Vector3 playerInitPos;  //プレイヤーの初期座標
+	Vector3 playerInitVel;  //プレイヤーの初速度
 };
